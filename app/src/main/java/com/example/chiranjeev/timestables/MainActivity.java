a82748fd8c5d8910ceedb58892141362d1eeb2c0
Chiranjeev Thomas <chiranjeevcollege@gmail.com>
2018-10-15 17:32:42 +0530

    formatted comments

--- app/src/main/java/com/example/chiranjeev/timestables/MainActivity.java
+++ app/src/main/java/com/example/chiranjeev/timestables/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.chiranjeev.timestables;
-// Step 1 : Set put the seek bar and list view widgets in the layout and give them proper ID's
-// Step 2 : Declare SeekBar and ListView objects in the MainActivity class
-// Step 3 : define the seekBar object by using findViewById and set it's max value to 19 ( 1 less than the max it's supposed to go to )
-// Step 4 : define a method in which ,
-// an array and an arrayAdapter and connect both of them and finally connect the arrayAdapter with the list using the listView's setAdapter() method
-// Step 5 : in the onSeekBarChangeListener function , of the seekBar ,in the progressChanged module put in the code for the declaration and initialization for the table
+/**
+ * Step 1 :  Set put the seek bar and list view widgets in the layout and give them proper ID's
+ * Step 2 : Declare SeekBar and ListView objects in the MainActivity class
+ * Step 3 : define the seekBar object by using findViewById and set it's max value to 19 ( 1 less than the max it's supposed to go to )
+ * Step 4 : define a method in which ,
+ * an array and an arrayAdapter and connect both of them and finally connect the arrayAdapter with the list using the listView's setAdapter() method
+ * Step 5 : in the onSeekBarChangeListener function , of the seekBar ,in the progressChanged module put in the code for the declaration and initialization for the table
+ */
+
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
